0c8e58d10a4a8089902004799bf083c08eeda572
sensei100 <jenanton01@gmail.com>
2016-11-21 10:53:58 -0500

    saved some unsaved files and set up rails routes

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :content, :user_id
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :books, only: [:create, :destroy, :update, :index, :show]
+  resources :posts, only: [:create, :destroy, :update, :index, :show]
+
 end
