2cfcb25647c0493cac95462a25159a6ba6b2ac4b
sensei100 <jenanton01@gmail.com>
2016-11-21 13:08:56 -0500

    bootstrap is working

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module CoverToCover
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-  config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap","assets","fonts")
-  config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
   end
 end
