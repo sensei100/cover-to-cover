37bb3168709f19d00b1886394a486e51af216eed
sensei100 <jenanton01@gmail.com>
2016-11-21 12:56:26 -0500

    created root route and action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   respond_to :json
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def index
+  end
+
   protected
 
   def configure_permitted_parameters
