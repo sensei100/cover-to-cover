c3d451fa224a86d64cc9147996349b8f16277ce6
sensei100 <jenanton01@gmail.com>
2016-11-26 07:54:35 -0500

    added current user to profile controller

--- app/assets/javascripts/profile/profile.controller.js
+++ app/assets/javascripts/profile/profile.controller.js
@@ -1,11 +1,30 @@
 (function() { 
   'use strict';
 
-  function ProfileController($scope) {
+  function ProfileController($scope, Auth) {
+
+    var ctrl = this;
+    ctrl.signedIn = Auth.isAuthenticated();
+
+    activate();
+
+    function activate() {
+      getCurrentUser();
+    }
+
+    function getCurrentUser() {
+      return Auth.currentUser()
+        .then(setCurrentUser);
+        }
+
+        function setCurrentUser(user) {
+          console.log(user);
+            return ctrl.user = user;
+        }
     
   }
 
-  ProfileController.$inject = ['$scope']
+  ProfileController.$inject = ['$scope', 'Auth']
 
 angular
   .module('app')
