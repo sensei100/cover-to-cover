4961c6e21d6bed62895e06a5af4f174a244b8b3f
sensei100 <jenanton01@gmail.com>
2016-11-26 18:38:50 -0500

    created post factory

---
+++ app/assets/javascripts/discussion/post.faactory.js
@@ -0,0 +1,79 @@
+(function() {
+
+  'use strict';
+
+  function PostFactory($http, $stateParams) {
+
+    var o = {
+      posts: []
+    };
+
+    return o;
+    
+    return {
+      getPosts: getPosts,
+      getPost: getPost,
+      // getUserPost: getUserPosts,
+      createPost: createPost,
+      updatePost: updatePost,
+      deletePost: deletePost,
+    }
+
+    function getPosts() {
+      return $http.get('/posts')
+        .then(handleResponse)
+        .catch(handleError)
+    }
+
+    function createPost(post) {
+      var req = {
+        method: 'POST',
+        url: '/posts',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: { 
+          post: post
+        }
+      };
+
+        return $http(req)
+          .catch(handleError);
+        }
+
+    function getPost(id) {
+      return $http.get('posts/' + $stateParams.id)
+        .then(handleResponse)
+        .catch(handleError);
+    }
+
+    function getUserPost() {
+
+    }
+
+    function updatePost() {
+
+    }
+
+    function deletePost() {
+
+    }
+    
+    function handleResponse(response) {
+      console.log(response)
+      return response.data
+    }
+
+    function handleError(error) {
+      console.log(error) 
+    }
+
+  }
+
+  PostFactory.$inject = ['$http', '$stateParams']
+
+  angular
+    .module('app')
+    .factory('PostFactory', PostFactory)
+
+}());
--- app/assets/javascripts/discussion/posts.controller.js
+++ app/assets/javascripts/discussion/posts.controller.js
@@ -2,13 +2,6 @@
   'use strict';
 
   function PostsController($scope) {
-    $scope.posts = [
-    'post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5',
-    ];
 
     $scope.addPost = function() {
       $scope.posts.push({
