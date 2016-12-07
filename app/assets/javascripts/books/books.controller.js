6f932d7cf804ab0ba94a7e43bf7b23ec3a17f370
sensei100 <jenanton01@gmail.com>
2016-12-06 19:53:21 -0500

    trying to access book data

--- app/assets/javascripts/books/books.controller.js
+++ app/assets/javascripts/books/books.controller.js
@@ -6,20 +6,25 @@
     var vm = this;
     vm.books;
     vm.getBooks = getBooks;
+    vm.bookSearch = bookSearch;
     vm.bookDetails = bookDetails;
     vm.signedIn = Auth.isAuthenticated();
     vm.searchTerm = '';
     vm.createBook = createBook;
     $scope.$state = $state;
 
+    console.log(vm.books);
+
     $scope.goBack = function() { 
       window.history.back();
     };
     console.log(Auth.isAuthenticated()); 
+
     activate();
 
     function activate() {
       getCurrentUser();
+      getBooks();
     }
 
     function getCurrentUser() {
@@ -32,25 +37,32 @@
             return vm.user = user;
         }
 
+    function getBooks() {
+      return BookFactory.getBooks()
+        .then(setBooks)
+    }
+
+    function setBooks(data) {
+      return vm.books = data;
+    }
     
     function createBook() {
       return BookFactory.createBook(vm.book, vm.user)
-
         .then(showBook)
       
       }
 
     function showBook(data) {
+      vm.books.push(data.book)
       $state.go('home.books')
     }
 
-    function getBooks() {
+    function bookSearch() {
       BookService
       .getBooks(vm.searchTerm)
       .then(function (response) {
         console.log(response.data.items);
-        $scope.items = response.data.items;
-        
+        $scope.items = response.data.items;  
       });
     }
 
