4823b0f5fb1f201a88e702e648415f56d407e883
sensei100 <jenanton01@gmail.com>
2016-12-07 14:43:55 -0500

    working on search filter

--- app/assets/javascripts/home/home.controller.js
+++ app/assets/javascripts/home/home.controller.js
@@ -1,11 +1,18 @@
 (function() { 
   'use strict';
 
-  function HomeController($scope) {
-    $scope.name = "test"
+  function HomeController($scope, $filter, Auth) {
+    var vm = this;
+    vm.signedIn = Auth.isAuthenticated();
+    vm.search = '';
+    vm.searchRating = '';
+
+    function refilter() {
+
+    }
   }
 
-  HomeController.$inject = ['$scope']
+  HomeController.$inject = ['$scope', '$filter', 'Auth']
 
 angular
   .module('app')
