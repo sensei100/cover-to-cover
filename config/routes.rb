e542a3fceeaffdecf8c2653d39398b66dbb046f9
sensei100 <jenanton01@gmail.com>
2016-12-20 08:08:18 -0500

    added route for upvote

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   root 'application#index'
   
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 
   resources :users, only: [:show, :edit]
   resources :books, only: [:create, :destroy, :update, :index, :show]
-  resources :posts, only: [:create, :destroy, :update, :index, :show] 
+  resources :posts, only: [:create, :destroy, :update, :index, :show] do
+    member do
+            put '/upvote' => 'posts#upvote'
+        end
+    end
 end
