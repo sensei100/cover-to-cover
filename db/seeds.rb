10aa79df05acd4a999b6ef3791b4b02b908a530a
sensei100 <jenanton01@gmail.com>
2016-12-22 19:51:47 -0500

    added seed data and trying to get google fonts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create([
+  {
+    title: "Horten's Miraculous Mechanisms", 
+    author: "Lissa Evans",
+    review: "This is a delightful children's book.  It is filled with adventure and mystery.", 
+    rating: "5", 
+    created_at: "2016-12-05 21:28:16"
+    },
+    {
+    title: "Harry Potter and the Cursed Child", 
+    author: "J.K. Rowling",
+    review: "I am a huge Harry Potter fan, but I had a hard time reading this as a play.", 
+    rating: "4", 
+    created_at: "2016-12-05 21:29:56"
+    },
+    {
+    title: "After Anna", 
+    author: "Alex Lake",
+    review: "This book is your typical suspenseful thriller. Nothing too exciting.", 
+    rating: "3", 
+    created_at: "2016-12-09 16:42:28"
+    },
+    {
+    title: "Life after Life", 
+    author: "Kate Atkinson",
+    review: "I had to read this for a book club and it could not have been more boring. It starts over again and again...", 
+    rating: "2", 
+    created_at: "2016-12-09 16:40:53"
+    },
+    {
+    title: "The Christmas Secret", 
+    author: "Wanda E. Brunstetter",
+    review: "I did not enjoy this book at all.  The storyline was completely predictable.", 
+    rating: "1", 
+    created_at: "2016-12-09 14:14:07"
+  }
+  ])
+
+
+
+
+
